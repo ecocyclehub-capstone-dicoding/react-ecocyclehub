2794c05490d45d9ea675cfd71dfa47d5ad2bd13c
Dika Saputra <69504128+RKaSaputra@users.noreply.github.com>
2026-05-14 15:00:36 +0700

    feat(transaction): add transaction management module

---
+++ src/entities/transaction/api/transaction.api.js
@@ -0,0 +1,21 @@
+import { axiosInstance } from "@/shared/api/axiosInstance";
+
+export const transactionApi = {
+  getAll: async () => {
+    const res = await axiosInstance.get("/transactions/");
+    return res.data;
+  },
+
+  create: async (payload) => {
+    const res = await axiosInstance.post("/transactions/", payload);
+    return res.data;
+  },
+
+  verify: async (transactionId) => {
+    const res = await axiosInstance.post(
+      `/transaction/${transactionId}/verify`,
+    );
+
+    return res.data;
+  },
+};
