784f887111dc7750ae88d310ceb761fadf6def69
Dika Saputra <tdikasaputraz@gmail.com>
2026-05-16 13:55:32 +0700

    feat: add auth provider

--- src/app/provider/index.jsx
+++ src/app/provider/index.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 
+import { AuthProvider } from "@/entities/auth/provider/AuthProvider";
+
 const AppProvider = () => (
-  //   <AuthProvider>
-  <RouterProvider router={router} />
-  //   </AuthProvider>
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 );
 
 export default AppProvider;
